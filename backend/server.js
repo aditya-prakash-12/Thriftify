00658db2df51dc5afdfc428dc16de9231fef2021
aditya-prakash-12 <adisingh841207@gmail.com>
2026-05-04 20:23:13 +0530

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB().catch(err => console.error("Failed to connect:", err.message));
 const app = express();
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "http://localhost:5177"], // frontend URLs
+  origin: "https://thriftify-ebon.vercel.app/" , // frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true // if you are sending cookies
 }));
